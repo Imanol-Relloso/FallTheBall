313f06716dc8e765e5f8e303c98e06d629cff1c3
Imanol Relloso Hernando <imanol_relloso@euneiz.com>
2026-09-15 02:58:56 +0200

    Actividad Popup terminada

---
+++ Assets/PopUp/Scripts/UIWindow.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class UIWindow : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    public virtual void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public virtual void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
