2bfbf63ec1e91676412cac99f484c4d26f7b0191
Imanol Relloso Hernando <imanol_relloso@euneiz.com>
2026-09-23 00:43:40 +0200

    ClaseUI

--- Assets/PopUp/Scripts/PopupUi.cs
+++ Assets/PopUp/Scripts/PopupUi.cs
@@ -1,5 +1,7 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class PopupUi : UIWindow
 {
+
 }
--- Assets/PopUp/Scripts/UIWindow.cs
+++ Assets/PopUp/Scripts/UIWindow.cs
@@ -1,18 +1,81 @@
+using DG.Tweening;
+using NaughtyAttributes;
 using UnityEngine;
 
 public class UIWindow : MonoBehaviour
 {
+    [SerializeField]
+    private RectTransform canvasRectTransform;
+    [SerializeField]
+    private CanvasGroup canvasGroup;
+    [SerializeField]
+    private bool hideOnStart;
+    
+    [Header("Animation Settings")]
+    [SerializeField]
+    protected float showDuration = 0.5f;    
+    [SerializeField]
+    protected float hideDuration = 0.5f; 
+    
+    [SerializeField]
+    protected Ease showEase = Ease.OutBack;    
+    [SerializeField]
+    protected Ease hideEase = Ease.InBack;
+    
+    
+    public CanvasGroup CanvasGroup => canvasGroup;
+    
     void Start()
     {
+        Initialize();
+    }
+    
+    public virtual void Initialize()
+    {
+        if(hideOnStart)
+            Hide();
+    }
+    
+    public virtual void Show(bool instant = false)
+    {
+        if(instant)
+            canvasRectTransform.gameObject.SetActive(true);
+        else
+        {
+            canvasRectTransform.gameObject.SetActive(true);
+                
+            RectTransform rectTran = canvasGroup.GetComponent<RectTransform>();
+
+            rectTran.DOScale(Vector3.one, showDuration).SetEase(showEase); 
+        }
+        
     }
 
-    public virtual void Show()
+    public virtual void Hide(bool instant = false)
     {
-        gameObject.SetActive(true);
+        if(instant)
+            canvasRectTransform.gameObject.SetActive(false);
+        else
+        {
+            RectTransform rectTran = canvasGroup.GetComponent<RectTransform>();
+
+            rectTran.DOScale(Vector3.zero, hideDuration).SetEase(hideEase).OnComplete(
+                ()=> canvasRectTransform.gameObject.SetActive(false)); 
+        }
+    }
+    
+    #region Test
+
+    [Button]
+    private void ShowTest()
+    {
+        Show();
+    }    
+    [Button]
+    private void HideTest()
+    {
+        Hide();
     }
 
-    public virtual void Hide()
-    {
-        gameObject.SetActive(false);
-    }
+    #endregion
 }
